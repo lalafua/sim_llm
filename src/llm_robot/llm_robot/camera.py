126006e9ec2d53aee2dac45aa96d8f3ac26ae5ab
lalafua <readme.llf@outlook.com>
2025-01-19 01:12:37 +0800

    Update

--- src/llm_robot/llm_robot/camera.py
+++ src/llm_robot/llm_robot/camera.py
@@ -1,52 +1,64 @@
-import rclpy
+import rclpy, threading
 from rclpy.node import Node
 from std_msgs.msg import String
 from rclpy.executors import MultiThreadedExecutor
-from inference import InferencePipeline
+from roboflow import Roboflow
 import cv2, os
 
 class cameraNode(Node):
     def __init__(self, name):
         super().__init__(name)
         self.get_logger().info("Node {} has been created.".format(name))    
-        self.camera_publisher_ = self.create_publisher(msg_type=String, topic="/camera/recognized", qos_profile=10)
-        self.timer_ = self.create_timer(0.3, self.timer_callback)
-        self.detection = {"class_name": [], "confidence": []}
-    
+        self.camera_publisher_ = self.create_publisher(String, "/camera/recognized", 10)
+        self.timer_ = self.create_timer(0.5, self.timer_callback)
+        self.detection = {"class_name": "", "confidence": 0.0}
+
     def timer_callback(self):
         msg = String()
-        # msg.data = "bottle"
         if self.detection["class_name"]:
-            msg.data = self.detection["class_name"]
-            self.detection["class_name"] = []
+            msg.data = f"Class: {self.detection['class_name']}"
+            self.detection["class_name"] = ""
             self.camera_publisher_.publish(msg)
-            self.get_logger().info("Published to topic '/camera/recognized': '{}'".format(msg.data))
-    
-    def my_sink(self, result, video_frame):
-        if result.get("output_image"):  
-            cv2.waitKey(1)
+            self.get_logger().info(f"Published to topic '/camera/recognized': '{msg.data}'")
 
-        predictions = result.get("predictions")
-        if predictions and len(predictions.confidence) > 0 and len(predictions.data["class_name"]) > 0 and predictions.confidence[0] > 0.5:
-            self.detection["class_name"] = predictions.data["class_name"][0]
-            self.detection["confidence"] = predictions.confidence[0]
+    def update_detection(self, predictions):
+        if predictions:
+            self.detection["class_name"] = predictions[0]['class']
+            self.detection["confidence"] = predictions[0]['confidence']
+
+class camera():
+    def __init__(self, camera_node): 
+        self.cap = cv2.VideoCapture(0)
+        self.frame = None
+        self.predictions = []
+        self.camera_node = camera_node
+
+        # Roboflow project init
+        rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
+        project = rf.workspace("buildmyownx").project("bottle-fviyh")
+        self.model = project.version(2).model
+
+    def process_frames(self):
+        while True:
+            ret, self.frame = self.cap.read()
+            if not ret:
+                print("Failed to grab frame")
+                break
+            if self.frame is not None:
+                self.predictions = self.model.predict(self.frame, confidence=50, overlap=50).json()['predictions']
+                self.camera_node.update_detection(self.predictions)
 
 def main(args=None):
     rclpy.init(args=args)
     camera_node = cameraNode("camera_node")
 
-    # pipeline object init
     try:
-        pipeline = InferencePipeline.init_with_workflow(
-            api_key=os.getenv("ROBOFLOW_API_KEY"),
-            workspace_name="buildmyownx",
-            workflow_id="custom-workflow",
-            video_reference=1, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
-            max_fps=30,
-            on_prediction=camera_node.my_sink
-        )
+        cam = camera(camera_node)
+        process_thread = threading.Thread(target=cam.process_frames)
+        process_thread.start()
+        process_thread.join()
     except Exception as e:
-        camera_node.get_logger().error("Error: {}".format(e))
+        camera_node.get_logger().error(f"Error: {e}")
         return
 
     executor = MultiThreadedExecutor()
@@ -54,13 +66,11 @@ def main(args=None):
 
     try:
         executor.spin()
-        pipeline.start()
-        pipeline.join()
+        camera_node.get_logger().info("Init success")   
     except KeyboardInterrupt:
         pass        
     finally:
         camera_node.destroy_node()
-        pipeline.terminate()
         rclpy.shutdown()
 
 if __name__ == "__main__":
